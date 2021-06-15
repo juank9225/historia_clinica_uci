c121b58dc03eea97603aa6192d8c846e668eee15
Juan carlos salcedo <juank9225@gmail.com>
2021-06-15 13:32:21 -0500

    Se crean los comandos de actualizar contado del paciente y acompañante del paciente

--- src/main/java/co/com/sofka/historia/historiaClinicaMedica/commands/actualizarAcompanante.java
+++ src/main/java/co/com/sofka/historia/historiaClinicaMedica/commands/actualizarAcompanante.java
@@ -1,4 +1,32 @@
 package co.com.sofka.historia.historiaClinicaMedica.commands;
 
-public class actualizarAcompanante {
+
+import co.com.sofka.domain.generic.Command;
+import co.com.sofka.historia.historiaClinicaMedica.values.Acompanante;
+import co.com.sofka.historia.historiaClinicaMedica.values.IdentificacionPaciente;
+import co.com.sofka.historia.historiaClinicaMedica.values.Ingreso;
+
+public class actualizarAcompanante implements Command {
+
+    private final Ingreso ingreso;
+    private final IdentificacionPaciente identificacionPaciente;
+    private final Acompanante acompanante;
+
+    public actualizarAcompanante(Ingreso ingreso, IdentificacionPaciente identificacionPaciente, Acompanante acompanante) {
+        this.ingreso = ingreso;
+        this.identificacionPaciente = identificacionPaciente;
+        this.acompanante = acompanante;
+    }
+
+    public Ingreso getIngreso() {
+        return ingreso;
+    }
+
+    public IdentificacionPaciente getIdentificacionPaciente() {
+        return identificacionPaciente;
+    }
+
+    public Acompanante getAcompanante() {
+        return acompanante;
+    }
 }
--- src/main/java/co/com/sofka/historia/historiaClinicaMedica/commands/actualizarContactoDePaciente.java
+++ src/main/java/co/com/sofka/historia/historiaClinicaMedica/commands/actualizarContactoDePaciente.java
@@ -1,4 +1,30 @@
 package co.com.sofka.historia.historiaClinicaMedica.commands;
 
-public class actualizarContactoDePaciente {
+import co.com.sofka.domain.generic.Command;
+import co.com.sofka.historia.historiaClinicaMedica.values.Contacto;
+import co.com.sofka.historia.historiaClinicaMedica.values.IdentificacionPaciente;
+import co.com.sofka.historia.historiaClinicaMedica.values.Ingreso;
+
+public class actualizarContactoDePaciente implements Command {
+    private final Ingreso ingreso;
+    private final IdentificacionPaciente entityId;
+    private final Contacto contacto;
+
+    public actualizarContactoDePaciente(Ingreso ingreso, IdentificacionPaciente entityId, Contacto contacto) {
+        this.ingreso = ingreso;
+        this.entityId = entityId;
+        this.contacto = contacto;
+    }
+
+    public Ingreso getIngreso() {
+        return ingreso;
+    }
+
+    public IdentificacionPaciente getEntityId() {
+        return entityId;
+    }
+
+    public Contacto getContacto() {
+        return contacto;
+    }
 }
