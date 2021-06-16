b95839968ab89f57f1c9c04b05a598058340f7cc
Juan carlos salcedo <juank9225@gmail.com>
2021-06-15 21:25:14 -0500

    Se crea la Entidad control de oxigeno del agregado historia clinica fisioterapia

--- src/main/java/co/com/sofka/historia/historiaClinicaFisioterapia/ControlDeOxigeno.java
+++ src/main/java/co/com/sofka/historia/historiaClinicaFisioterapia/ControlDeOxigeno.java
@@ -1,4 +1,63 @@
 package co.com.sofka.historia.historiaClinicaFisioterapia;
 
-public class ControlDeOxigeno {
+import co.com.sofka.domain.generic.Entity;
+import co.com.sofka.historia.historiaClinicaFisioterapia.values.*;
+import co.com.sofka.historia.historiaClinicaMedica.values.Fecha;
+
+import java.util.Objects;
+
+public class ControlDeOxigeno extends Entity<NumeroControlOxigeno> {
+
+    private Fecha fecha;
+    private InicioControlOxigeno inicioControlOxigeno;
+    private TerminacionControlOxigeno terminacionControlOxigeno;
+    private Suministro suministro;
+    private LitrosOxigeno litrosOxigeno;
+    private ModoAdministracion modoAdministracion;
+
+    public ControlDeOxigeno(NumeroControlOxigeno entityId, Fecha fecha, InicioControlOxigeno inicioControlOxigeno, TerminacionControlOxigeno terminacionControlOxigeno, Suministro suministro, LitrosOxigeno litrosOxigeno, ModoAdministracion modoAdministracion) {
+        super(entityId);
+        this.fecha = fecha;
+        this.inicioControlOxigeno = inicioControlOxigeno;
+        this.terminacionControlOxigeno = terminacionControlOxigeno;
+        this.suministro = suministro;
+        this.litrosOxigeno = litrosOxigeno;
+        this.modoAdministracion = modoAdministracion;
+    }
+
+    public void actualizarSuministro(Suministro suministro){
+        this.suministro = Objects.requireNonNull(suministro);
+    }
+
+    public void actualizarLitrosDeOxigeno(LitrosOxigeno litrosOxigeno){
+        this.litrosOxigeno = Objects.requireNonNull(litrosOxigeno);
+    }
+
+    public void actualizarModoAdministracion(ModoAdministracion modoAdministracion){
+        this.modoAdministracion = Objects.requireNonNull(modoAdministracion);
+    }
+
+    public Fecha Fecha() {
+        return fecha;
+    }
+
+    public InicioControlOxigeno tInicioControlOxigeno() {
+        return inicioControlOxigeno;
+    }
+
+    public TerminacionControlOxigeno TerminacionControlOxigeno() {
+        return terminacionControlOxigeno;
+    }
+
+    public Suministro Suministro() {
+        return suministro;
+    }
+
+    public LitrosOxigeno LitrosOxigeno() {
+        return litrosOxigeno;
+    }
+
+    public ModoAdministracion ModoAdministracion() {
+        return modoAdministracion;
+    }
 }
